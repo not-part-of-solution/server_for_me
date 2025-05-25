ed80e3ab5b44bf1a391dad98e6616a191ecbd3fb
not-part-of-solution <aeztva@gmail.com>
2025-05-25 22:50:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def predict():
     return jsonify({'class': label, 'confidence': confidence})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
